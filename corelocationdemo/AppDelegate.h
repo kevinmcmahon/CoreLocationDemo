4020e9072623fb211a693a185c6e2d24b3bf8738
Kevin McMahon <klmcmahon+github@gmail.com>
2012-08-12 09:23:35 -0500

    Initial Commit

---
+++ corelocationdemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  corelocationdemo
+//
+//  Created by Kevin McMahon on 8/12/12.
+//  Copyright (c) 2012 Kevin McMahon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ corelocationdemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  corelocationdemo
+//
+//  Created by Kevin McMahon on 8/12/12.
+//  Copyright (c) 2012 Kevin McMahon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
